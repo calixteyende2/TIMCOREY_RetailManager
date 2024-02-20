0031efa531194471d1528ab3f0bcb5165fd7cd1d
calixte <yendecalixte@yahoo.fr>
2024-02-20 16:06:12 -0500

    Creation des tables

---
+++ TRMDataBase/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [saleId] INT NOT NULL, 
+    [ProductId] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    [Quantity] INT NOT NULL DEFAULT 1
+)
